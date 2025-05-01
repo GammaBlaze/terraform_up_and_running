07259e4671a818e5b84d46402bdd58804ff6aa10
GammaBlaze <gammablaze@gmail.com>
2025-05-01 20:56:08 +0100

    Adding chapter 4 code.

---
+++ chapter4/live/stage/services/webserver-cluster/main.tf
@@ -0,0 +1,29 @@
+# Partial configuration. The other settings (e.g., bucket, region) will be
+# passed in from a file via -backend-config arguments to 'terraform init'
+terraform {
+  backend "s3" {
+    key = "stage/services/webserver-cluster/terraform.tfstate"
+  }
+}
+
+module "webserver_cluster" {
+  source = "github.com/GammaBlaze/modules//services/webserver-cluster"
+
+  cluster_name           = "webservers-stage"
+  db_remote_state_bucket = "the-bucket-residence-state"
+  db_remote_state_key    = "stage/data-stores/my-sql/terraform.tfstate"
+
+  instance_type = "t2.micro"
+  min_size      = 2
+  max_size      = 2
+}
+
+resource "aws_security_group_rule" "allow_testing_inbound" {
+  type              = "ingress"
+  security_group_id = module.webserver_cluster.alb_security_group_id
+
+  from_port   = 12345
+  to_port     = 12345
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
